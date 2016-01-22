fd31469bb2a95ac414787d77399e13777ad6808c
Frank Ibem <soulsolja@yahoo.com>
2016-01-21 22:52:47 -0600

    Added example

---
+++ k-means-imaging/Examples/Examples.cs
@@ -0,0 +1,25 @@
+﻿using Algorithms;
+using k_means_imaging.Imaging;
+using System.Drawing;
+
+namespace k_means_imaging.Examples
+{
+    public class Examples
+    {
+        public void Example1()
+        {
+            // Read in the image
+            string imageLink = @"Input Image Link Here";
+            Bitmap bitmap = new Bitmap(imageLink);
+
+            // Map, run K-Means and recover image
+            var mappedValues = KMeansAdapter.MapToKMeans(bitmap);
+            var kMeansResult = KMeans.Run(mappedValues, 10, true, 10);
+            var result = KMeansAdapter.RecoverFromKMeans(kMeansResult.Item1,
+                kMeansResult.Item2, bitmap.Height, bitmap.Width);
+
+            // Save the result
+            result.Save(@"Output Image Link Here");
+        }
+    }
+}
